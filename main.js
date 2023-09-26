bb41bfa0a2a4ad8ada7317ec6aa2cf14b50ed59f
David van Zeventer <d.van.zeventer@connectingthedots.nl>
2023-09-26 14:16:49 +0200

    wip

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.',
+            
+            bla: 'ddd'
+            // Solution
+        }
+    }
+})
